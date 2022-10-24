c3dee73ea19ab83c07347c32dbabb42230ee6a0c
maceyjshort <macey.walker1991@gmail.com>
2022-10-24 11:59:08 -0500

    Make note commit to myself

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 
 function App() {
   // TODO: Add useReducer hook to track state of: board, turn, winner
+  // useReducer good when multiple peices of state changing at same time
 
   return <h1>Tic Tac Toe</h1>;
 }
