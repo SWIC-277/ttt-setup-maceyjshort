1c68cf5d3d233c49776a12137b3c92b5dbfee205
maceyjshort <macey.walker1991@gmail.com>
2022-10-24 22:22:41 -0500

    Start reducer like video

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ";made_move": {
+    }
+  }
+}
 
 function App() {
   // TODO: Add useReducer hook to track state of: board, turn, winner
+  const [state, dispatch] = useReducer(reducer, {
+    board: Array[9].fill[""],
+    turn: "X",
+    winner: null,
+  });
   // useReducer good when multiple peices of state changing at same time
 
   return <h1>Tic Tac Toe</h1>;
