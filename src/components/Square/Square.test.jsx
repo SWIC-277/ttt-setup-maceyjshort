37ae35705a9e951268ed4c020f9945bf3969e8e0
Macey Short <macey.walker1991@gmail.com>
2022-10-26 18:48:04 -0500

    add code

--- src/components/Square/Square.test.jsx
+++ src/components/Square/Square.test.jsx
@@ -1,10 +1,11 @@
 import { render, screen } from "@testing-library/react";
-import Square from "./Sqaure";
+import Square from "./Square";
 
 it("renders a square with the correct marker('X' or 'O')", () => {
   render(<Square marker="X" />);
 
-  const squre = screen.getByRole("button", { name: "X" });
+  const square = screen.getByRole("button", { name: "X" });
 
-  expect(squre).toBeInTheDocument();
+  expect(square).toHaveAttribute("id", "0");
+  expect(square).toBeInTheDocument();
 });
