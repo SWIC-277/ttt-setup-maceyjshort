40b5946879b4a81e48f6e3b51b297f6a80b4235f
maceyjshort <macey.walker1991@gmail.com>
2022-10-31 20:19:15 -0500

    Update board.map

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import Square from "./components/Square/Square";
 import useGame from "./hooks/useGame";
 
 function App() {
-  const { board, makeMove, winner, turn } = useGame();
+  const { board, winner, makeMove } = useGame();
   return (
     <main>
       <div className="board">
         {board.map((square, index) => (
-          <Square key={index} id={index} handleClick={() => {}} />
+          <Square
+            key={index}
+            id={index}
+            handleClick={makeMove}
+            marker={square}
+          />
         ))}
       </div>
     </main>
