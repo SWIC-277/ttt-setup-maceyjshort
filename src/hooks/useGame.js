ccccdbefc91aae3fa11f05d7928ece437838b331
maceyjshort <macey.walker1991@gmail.com>
2022-10-31 20:17:44 -0500

    Move reducer to new doc

--- src/hooks/useGame.js
+++ src/hooks/useGame.js
@@ -1,26 +1,5 @@
 import { useReducer } from "react";
-import check4Winner from "services/game.service";
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "made_move": {
-      // Only mutate a local copy of the state
-      const board2Update = [...state.board];
-
-      if (!board2Update[action.index]) {
-        board2Update[action.index] = state.turn;
-      }
-
-      return {
-        board: board2Update,
-        turn: state.turn === "X" ? "O" : "X",
-        winner: check4Winner(board2Update, state.turn) ? state.turn : null,
-      };
-    }
-    default:
-      throw new Error("Invalid action");
-  }
-}
+import reducer from "./reducer";
 
 export default function useGame() {
   const [state, dispatch] = useReducer(reducer, {
