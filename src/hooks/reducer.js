bd6fa3f0eb8abe227c82c3f3b9d6cb900cfff074
maceyjshort <macey.walker1991@gmail.com>
2022-11-04 21:23:34 -0500

    Fix bug for turn error

--- src/hooks/reducer.js
+++ src/hooks/reducer.js
@@ -5,14 +5,16 @@ export default function reducer(state, action) {
     case "made_move": {
       // Only mutate a local copy of the state
       const board2Update = [...state.board];
+      let updatedTurn = state.turn;
 
       if (!board2Update[action.index]) {
         board2Update[action.index] = state.turn;
+        updatedTurn = state.turn === "X" ? "O" : "X";
       }
 
       return {
         board: board2Update,
-        turn: state.turn === "X" ? "O" : "X",
+        turn: updatedTurn,
         winner: check4Winner(board2Update, state.turn) ? state.turn : null,
       };
     }
